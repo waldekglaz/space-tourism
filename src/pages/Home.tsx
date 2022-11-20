a330a886e88b56bccc3efca1fb16ceb1e929af5a
Waldemar Glaz <ania@Waldemars-MacBook-Pro.local>
2022-11-20 18:03:09 +0100

    Home styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,22 +1,29 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import styles from "./Home.module.css";
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
   return (
-    <main>
+    <main className={styles.home}>
       <header>
         <Navbar />
       </header>
-      <div className="text-content">
-        <div className="eyebrow">SO, YOU WANT TO TRAVEL TO</div>
-        <h1 className="title">SPACE</h1>
-        <div className="text">
+      <div className={styles["text-content"]}>
+        <div className={styles.eyebrow}>SO, YOU WANT TO TRAVEL TO</div>
+        <h1 className={styles.title}>SPACE</h1>
+        <div className={styles.text}>
           Let’s face it; if you want to go to space, you might as well genuinely
           go to outer space and not hover kind of on the edge of it. Well sit
           back, and relax because we’ll give you a truly out of this world
           experience!
         </div>
       </div>
+      <Link to="/destination">
+        <button type="button" className={styles.home__btn}>
+          EXPLORE
+        </button>
+      </Link>
     </main>
   );
 };
