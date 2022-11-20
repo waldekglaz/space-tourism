c52b5d59bc1a5960d0264b3bc36e8d61587b291e
Waldemar Glaz <ania@Waldemars-MacBook-Pro.local>
2022-11-20 17:18:56 +0100

    Home jsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,6 +7,16 @@ const Home: React.FC = () => {
       <header>
         <Navbar />
       </header>
+      <div className="text-content">
+        <div className="eyebrow">SO, YOU WANT TO TRAVEL TO</div>
+        <h1 className="title">SPACE</h1>
+        <div className="text">
+          Let’s face it; if you want to go to space, you might as well genuinely
+          go to outer space and not hover kind of on the edge of it. Well sit
+          back, and relax because we’ll give you a truly out of this world
+          experience!
+        </div>
+      </div>
     </main>
   );
 };
