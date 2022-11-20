f3cd1561b18db5819f03c304b6cb8b1f664a7b17
Waldemar Głaz <w.glaz@cheil.com>
2022-11-20 21:52:30 +0100

    mobile menu styles

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,30 +2,34 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../assets/shared/logo.svg";
 import hamburger from "../assets/shared/icon-hamburger.svg";
+import closeIcon from "../assets/shared/icon-close.svg";
 import styles from "./Navbar.module.css";
 
 const Navbar: React.FC = () => {
   return (
     <div className={styles.navbar}>
-      <img src={logo} alt="logo" className="logo" />
+      <img src={logo} alt="logo" className={styles.logo} />
       <ul>
-        <li className="list-item">
-          <Link to="/">
+        <button>
+          <img src={closeIcon} alt="close" className={styles["close-icon"]} />
+        </button>
+        <li className={styles["list-item"]}>
+          <Link to="/" className={styles["navbar__link"]}>
             <span className="list-item__index">00</span>Home
           </Link>
         </li>
-        <li className="list-item">
-          <Link to="/destination">
+        <li className={styles["list-item"]}>
+          <Link to="/destination" className={styles["navbar__link"]}>
             <span className="list-item__index">01</span>Destination
           </Link>
         </li>
-        <li className="list-item">
-          <Link to="/crew">
+        <li className={styles["list-item"]}>
+          <Link to="/crew" className={styles["navbar__link"]}>
             <span className="list-item__index">02</span>crew
           </Link>
         </li>
-        <li className="list-item">
-          <Link to="/technology">
+        <li className={styles["list-item"]}>
+          <Link to="/technology" className={styles["navbar__link"]}>
             <span className="list-item__index">03</span>technology
           </Link>
         </li>
